d94edd2663e711d7bc409618fce7df6661660daa
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-05-30 16:48:52 +0100

    feat(vscode): basic boilerplate to setup lsp

---
+++ packages/vscode/src/extension.ts
@@ -0,0 +1,56 @@
+import * as serverProtocol from '@volar/language-server/protocol'
+import { createLabsInfo, getTsdk } from '@volar/vscode'
+import * as vscode from 'vscode'
+import * as lsp from 'vscode-languageclient/node'
+
+let client: lsp.BaseLanguageClient
+
+export async function activate(context: vscode.ExtensionContext) {
+  const serverModule = vscode.Uri.joinPath(context.extensionUri, 'dist', 'server.js')
+  const runOptions = { execArgv: <string[]>[]}
+  const debugOptions = { execArgv: ['--nolazy', '--inspect=' + 6009] }
+  const serverOptions: lsp.ServerOptions = {
+    run: {
+      module: serverModule.fsPath,
+      transport: lsp.TransportKind.ipc,
+      options: runOptions
+    },
+    debug: {
+      module: serverModule.fsPath,
+      transport: lsp.TransportKind.ipc,
+      options: debugOptions
+    }
+  }
+  const clientOptions: lsp.LanguageClientOptions = {
+    documentSelector: [
+    { scheme: 'file', language: 'javascript' },
+    { pattern: '**/*.ejs' },
+    { pattern: '**/.sailsrc' }
+    ],
+    initializationOptions: {
+			typescript: {
+				tsdk: (await getTsdk(context)).tsdk,
+			},
+		},
+  }
+
+  client = new lsp.LanguageClient(
+    'sails-language-server',
+    'Sails Language Server',
+    serverOptions,
+    clientOptions
+  )
+
+  await client.start()
+
+  // support for https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volarjs-labs
+	// ref: https://twitter.com/johnsoncodehk/status/1656126976774791168
+	const labsInfo = createLabsInfo(serverProtocol);
+	labsInfo.addLanguageClient(client);
+	return labsInfo.extensionExports;
+
+}
+
+export function deactivate(): Thenable<any> | undefined {
+	return client?.stop();
+}
