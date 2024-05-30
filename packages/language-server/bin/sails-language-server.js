e1d2fe2e1b45c466200cbd57b721d38b5eeaf76e
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-05-30 18:04:37 +0100

    chore(language-server): ran prettier

--- packages/language-server/bin/sails-language-server.js
+++ packages/language-server/bin/sails-language-server.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
-if (process.argv.includes("--version")) {
-	const pkgJSON = require("../package.json");
-	console.log(`${pkgJSON["version"]}`);
-}
-else {
-	require("../out/index.js");
+if (process.argv.includes('--version')) {
+  const pkgJSON = require('../package.json')
+  console.log(`${pkgJSON['version']}`)
+} else {
+  require('../out/index.js')
 }
