72f45b30fb6c15f3e30d6b39cb5954217ccf47f6
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2025-05-19 15:59:08 +0100

    feat(language-server): update validateDocument to include typeMap for action validation

--- packages/language-server/validators/validate-document.js
+++ packages/language-server/validators/validate-document.js
@@ -1,11 +1,11 @@
 const validateAutomigrationStrategy = require('./validate-auto-migration-strategy')
 const validateActionExist = require('./validate-action-exist')
 
-module.exports = function validateDocument(connection, document) {
+module.exports = function validateDocument(connection, document, typeMap) {
   const diagnostics = []
 
   const modelDiagnostics = validateAutomigrationStrategy(document)
-  const actionDiagnostics = validateActionExist(document)
+  const actionDiagnostics = validateActionExist(document, typeMap)
 
   diagnostics.push(...modelDiagnostics, ...actionDiagnostics)
 
